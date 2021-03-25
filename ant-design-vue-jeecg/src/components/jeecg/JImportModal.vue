4559b387174e14497f8e1ac3b1140b7ee3a47be1
Qiuyi LI <qiuyi.li@gmail.com>
2021-03-25 17:20:08 +0100

    Fix grammar mistake

--- ant-design-vue-jeecg/src/components/jeecg/JImportModal.vue
+++ ant-design-vue-jeecg/src/components/jeecg/JImportModal.vue
@@ -61,12 +61,12 @@
     watch: {
       url (val) {
         if(val){
-         this.uploadAction = window._CONFIG['domianURL']+val
+         this.uploadAction = window._CONFIG['domainURL']+val
         }
       }
     },
     created () {
-      this.uploadAction = window._CONFIG['domianURL']+this.url
+      this.uploadAction = window._CONFIG['domainURL']+this.url
     },
 
     methods:{
