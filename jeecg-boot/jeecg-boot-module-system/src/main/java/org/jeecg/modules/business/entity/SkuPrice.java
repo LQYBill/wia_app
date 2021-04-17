899e2dddd1b4dbb88681b92644d6ed22b198de19
Wenke-D <matthieu.du@outlook.com>
2021-04-17 10:37:06 +0200

    feat: remodel sku price

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/business/entity/SkuPrice.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/business/entity/SkuPrice.java
@@ -1,6 +1,7 @@
 package org.jeecg.modules.business.entity;
 
 import java.io.Serializable;
+
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -11,62 +12,77 @@ import org.jeecgframework.poi.excel.annotation.Excel;
 
 import java.math.BigDecimal;
 import java.util.Date;
+
 import org.jeecg.common.aspect.annotation.Dict;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 /**
- * @Description: SKU价格表
+ * @Description: The price of a sku
  * @Author: jeecg-boot
- * @Date:   2021-04-16
+ * @Date: 2021-04-16
  * @Version: V1.0
  */
-@ApiModel(value="sku对象", description="SKU表")
+@ApiModel(value = "sku对象", description = "SKU表")
 @Data
 @TableName("sku_price")
 public class SkuPrice implements Serializable {
     private static final long serialVersionUID = 1L;
 
-	/**主键*/
-	@TableId(type = IdType.ASSIGN_ID)
+    /**
+     * id in the DB
+     */
+    @TableId(type = IdType.ASSIGN_ID)
     @ApiModelProperty(value = "主键")
     private String id;
-	/**创建人*/
-    @ApiModelProperty(value = "创建人")
-    private String createBy;
-	/**创建日期*/
-	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
-    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
-    @ApiModelProperty(value = "创建日期")
-    private Date createTime;
-	/**更新人*/
-    @ApiModelProperty(value = "更新人")
-    private String updateBy;
-	/**更新日期*/
-	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
-    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
-    @ApiModelProperty(value = "更新日期")
-    private Date updateTime;
-	/**SKU ID*/
-	@Dict(dictTable = "sku", dicText = "erp_code", dicCode = "id")
+
+    /**
+     * SKU ID
+     */
+    @Dict(dictTable = "sku", dicText = "erp_code", dicCode = "id")
     @ApiModelProperty(value = "SKU ID")
     private String skuId;
-	/**价格*/
-	@Excel(name = "价格", width = 15)
+
+    /**
+     * 价格
+     */
+    @Excel(name = "价格", width = 15)
     @ApiModelProperty(value = "价格")
     private BigDecimal price;
-	/**优惠价起订量*/
-	@Excel(name = "优惠价起订量", width = 15)
+
+    /**
+     * 优惠价起订量
+     */
+    @Excel(name = "优惠价起订量", width = 15)
     @ApiModelProperty(value = "优惠价起订量")
     private Integer threshold;
-	/**优惠价*/
-	@Excel(name = "优惠价", width = 15)
+
+    /**
+     * 优惠价
+     */
+    @Excel(name = "优惠价", width = 15)
     @ApiModelProperty(value = "优惠价")
     private BigDecimal discountedPrice;
-	/**生效日期*/
-	@Excel(name = "生效日期", width = 15, format = "yyyy-MM-dd")
-	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd")
-    @DateTimeFormat(pattern="yyyy-MM-dd")
+
+    /**
+     * 生效日期
+     */
+    @Excel(name = "生效日期", width = 15, format = "yyyy-MM-dd")
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @ApiModelProperty(value = "生效日期")
     private Date date;
+
+    /**
+     * The price of a sku depends on its quantity, Given a quantity here, return the correspondent price.
+     *
+     * @param quantity a quantity
+     * @return the price correspondent to the quantity
+     */
+    public BigDecimal getPrice(int quantity) {
+        if (quantity > threshold) {
+            return discountedPrice;
+        }
+        return price;
+    }
 }
