5cab5d7e3d471ccceb35fc7cf83a2cd4a29f40e3
Qiuyi LI <qiuyi.li@gmail.com>
2021-05-14 16:59:59 +0200

    Features : 1. Divide orders into 3 tabs : pending, purchasing, processed 2. Add badges to show order quantities in each tab 3. Extract pending orders into a new vue for re-usability

--- ant-design-vue-jeecg/src/views/business/client/platformOrder/ClientPlatformOrderList.vue
+++ ant-design-vue-jeecg/src/views/business/client/platformOrder/ClientPlatformOrderList.vue
@@ -1,325 +1,50 @@
 <template>
-  <a-card class="j-inner-table-wrapper" :bordered="false">
-    <template>
-      <a-row :gutter="16">
-        <a-col :span="6">
-          <a-statistic title="SKU Number" :value="orderData.skuNumber" style="margin-right: 50px">
-            <template #suffix>
-              <span>units</span>
-            </template>
-          </a-statistic>
-        </a-col>
-        <a-col :span="6">
-          <a-statistic title="Total Quantity" :value="orderData.totalQuantity" class="demo-class">
-            <template #suffix>
-              <span>units</span>
-            </template>
-          </a-statistic>
-        </a-col>
-        <a-col :span="6">
-          <a-statistic title="Estimated Price" :value="orderData.estimatedTotalPrice" class="demo-class">
-            <template #suffix>
-              <span>€</span>
-            </template>
-          </a-statistic>
-        </a-col>
-        <a-col :span="6">
-          <a-statistic title="Reduced Amount" :value="orderData.reducedAmount" class="demo-class">
-            <template #suffix>
-              <span>€</span>
-            </template>
-          </a-statistic>
-        </a-col>
-      </a-row>
-    </template>
-
-    <!-- 查询区域 begin -->
-    <div class="table-page-search-wrapper">
-      <a-form layout="inline">
-        <a-row :gutter="24">
-        </a-row>
-      </a-form>
-    </div>
-    <!-- 查询区域 end -->
-
-    <!-- table区域 begin -->
-    <div>
-
-      <a-alert type="info" showIcon style="margin-bottom: 16px;">
-        <template slot="message">
-          <span>{{ $t("operation.selected") }}</span>
-          <a style="font-weight: 600;padding: 0 4px;">{{ selectedRowKeys.length }}</a>
-          <span>{{ $t("order.orders") }}</span>
-          <a style="margin-left: 24px" @click="onClearSelected">{{ $t("operation.reset") }}</a>
-        </template>
-      </a-alert>
-
-
-      <a-table
-        ref="table"
-        size="middle"
-        bordered
-        rowKey="id"
-        class="j-table-force-nowrap"
-        :scroll="{ x: true}"
-        :loading="loading"
-        :columns="columns"
-        :dataSource="dataSource"
-        :pagination="ipagination"
-        :expandedRowKeys="expandedRowKeys"
-        :rowSelection="{selectedRowKeys, onChange: computeInfo}"
-        @expand="handleExpand"
-        @change="handleTableChange"
-      >
-
-        <!-- 内嵌table区域 begin -->
-        <template slot="expandedRowRender" slot-scope="record">
-          <a-tabs tabPosition="top">
-            <a-tab-pane tab="Order Contents" key="platformOrderContent" forceRender>
-              <order-content :record="record"/>
-            </a-tab-pane>
-          </a-tabs>
-        </template>
-        <!-- 内嵌table区域 end -->
-
-      </a-table>
-    </div>
-    <!-- table区域 end -->
-
-    <!-- 表单区域 -->
-    <popup-confirmation ref="popup" :ok-callback="modalFormOk" :data-for-child="selectedRowKeys"/>
-    <a-space class="bottomButtons">
-      <a-button type="danger" @click="onClearSelected">
-        Reset
-      </a-button>
-      <a-button type="primary" @click="handleOrder">
-        Place Order
-        <a-icon type="right"/>
-      </a-button>
-    </a-space>
-
-  </a-card>
+  <a-tabs default-active-key="1" @change="callback">
+    <a-tab-pane key="1">
+      <span slot="tab">
+        Pending orders <a-badge :count="109" :number-style="{ backgroundColor: '#fa541c' }" :overflow-count="999" style='margin-bottom: 5px'/>
+      </span>
+      <pending-order-list/>
+    </a-tab-pane>
+    <a-tab-pane key="2" force-render>
+      <span slot="tab">
+        Purchasing orders <a-badge count="109" :number-style="{ backgroundColor: '#faad14' }" :overflow-count="999" style='margin-bottom: 5px'/>
+      </span>
+      Content of Tab Pane 2
+    </a-tab-pane>
+    <a-tab-pane key="3">
+      <span slot="tab">
+        Processed orders <a-badge count="109" :number-style="{ backgroundColor: '#a0d911' }" :overflow-count="999" style='margin-bottom: 5px'/>
+      </span>
+      Content of Tab Pane 3
+    </a-tab-pane>
+  </a-tabs>
 </template>
 
 <script>
 
-import {JeecgListMixin} from '@/mixins/JeecgListMixin'
-import PopupConfirmation from './modules/ConfirmationContainer'
-import OrderContent from './subTables/OrderContent'
+import PendingOrderList from './PendingOrderList'
 
 import '@assets/less/TableExpand.less'
 
-const {postAction} = require("@api/manage");
-
 export default {
   name: 'PlatformOrderList',
-  mixins: [JeecgListMixin],
   components: {
-    PopupConfirmation,
-    OrderContent,
+    PendingOrderList
   },
   data() {
     return {
       description: 'Client platform order page',
-      // 表头
-      columns: [
-        {
-          title: '#',
-          key: 'rowIndex',
-          width: 60,
-          align: 'center',
-          customRender: (t, r, index) => parseInt(index) + 1
-        },
-        {
-          title: this.$t('storeID'),
-          align: 'center',
-          dataIndex: 'shopId_dictText',
-          width: 60,
-        },
-        {
-          title: this.$t('order.Num'),
-          align: 'center',
-          dataIndex: 'platformOrderId',
-          width: 120,
-        },
-        {
-          title: this.$t("order.transNum"),
-          align: 'center',
-          dataIndex: 'platformOrderNumber',
-          width: 60,
-          ellipsis: true,
-        },
-        {
-          title: this.$t('order.trackingNum'),
-          align: 'center',
-          dataIndex: 'trackingNumber',
-          width: 120,
-        },
-        {
-          title: this.$t("order.transTime"),
-          align: 'center',
-          dataIndex: 'orderTime',
-          width: 120,
-        },
-        {
-          title: this.$t("order.deliveryTime"),
-          align: 'center',
-          dataIndex: 'shippingTime',
-          width: 120,
-        },
-        {
-          title: this.$t("recipient.recipient"),
-          align: 'center',
-          dataIndex: 'recipient',
-          ellipsis: true,
-          width: 50
-        },
-        {
-          title: this.$t("recipient.country"),
-          align: 'center',
-          dataIndex: 'country',
-          width: 120,
-        },
-        {
-          title: this.$t("recipient.postalCode"),
-          align: 'center',
-          dataIndex: 'postcode',
-          width: 80,
-        },
-        {
-          title: this.$t("logistics.registerFee"),
-          align: 'center',
-          dataIndex: 'fretFee',
-          width: 80,
-        },
-        {
-          title: this.$t("logistics.invoiceNum"),
-          align: 'center',
-          dataIndex: 'shippingInvoiceNumber_dictText',
-          width: 147,
-        },
-        {
-          title: this.$t("status"),
-          align: 'center',
-          dataIndex: 'status_dictText',
-          width: 147,
-        },
-      ],
-      // 字典选项
-      dictOptions: {},
-      // 展开的行test
-      expandedRowKeys: [],
-      url: {
-        list: '/business/clientPlatformOrder/list',
-        delete: '/business/clientPlatformOrder/delete',
-        deleteBatch: '/business/clientPlatformOrder/deleteBatch',
-        exportXlsUrl: '/business/clientPlatformOrder/exportXls',
-        importExcelUrl: '/business/clientPlatformOrder/importExcel',
-        computeInfo: '/business/clientPlatformOrder/computeInfo'
-      },
-      superFieldList: [],
-      orderData: {
-        skuNumber: 0,
-        totalQuantity: 0,
-        estimatedPrice: 0,
-        reducedAmount: 0
-      }
     }
   },
   created() {
-    this.getSuperFieldList();
+
   },
   computed: {},
   methods: {
-    handleExpand(expanded, record) {
-      this.expandedRowKeys = []
-      if (expanded === true) {
-        this.expandedRowKeys.push(record.id)
-      }
-    },
-    getSuperFieldList() {
-      let fieldList = [];
-      fieldList.push({
-        type: 'sel_search',
-        value: 'shopId',
-        text: '店铺ID',
-        dictTable: 'shop',
-        dictText: 'erp_code',
-        dictCode: 'id'
-      })
-      fieldList.push({
-        type: 'sel_search',
-        value: 'logisticChannelName',
-        text: '物流渠道',
-        dictTable: 'logistic_channel',
-        dictText: 'zh_name',
-        dictCode: 'zh_name'
-      })
-      fieldList.push({type: 'string', value: 'platformOrderId', text: '平台订单号码', dictCode: ''})
-      fieldList.push({type: 'string', value: 'platformOrderNumber', text: '平台订单交易号', dictCode: ''})
-      fieldList.push({type: 'string', value: 'trackingNumber', text: '物流跟踪号', dictCode: ''})
-      fieldList.push({type: 'date', value: 'orderTime', text: '订单交易时间'})
-      fieldList.push({type: 'date', value: 'shippingTime', text: '订单发货时间'})
-      fieldList.push({type: 'string', value: 'recepient', text: '订单收件人', dictCode: ''})
-      fieldList.push({type: 'string', value: 'country', text: '订单收件人国家', dictCode: ''})
-      fieldList.push({type: 'string', value: 'postcode', text: '订单收件人邮编', dictCode: ''})
-      fieldList.push({type: 'BigDecimal', value: 'fretFee', text: '物流挂号费', dictCode: ''})
-      fieldList.push({
-        type: 'sel_search',
-        value: 'shippingInvoiceNumber',
-        text: '物流发票号',
-        dictTable: 'shipping_invoice',
-        dictText: 'invoice_number',
-        dictCode: 'id'
-      })
-      fieldList.push({type: 'string', value: 'status', text: '状态', dictCode: ''})
-      this.superFieldList = fieldList
-    },
-    computeInfo(selectedRowKeys, selectionRows) {
-      this.selectedRowKeys = selectedRowKeys;
-      this.selectionRows = selectionRows;
-
-      const params = this.selectedRowKeys
-      console.log(params)
-      if (params.length === 0) {
-        this.resetOrderData()
-      } else {
-        let self = this
-        postAction(this.url.computeInfo, params)
-          .then(
-            res => {
-              console.log(res.result)
-              self.orderData = res.result
-            }
-          )
-      }
-    },
-    handleOrder() {
-      if (this.selectedRowKeys.length === 0){
-        this.$message.warning("You should select at least 1 order to continue !", 5)
-        return
-      }
-      this.$refs.popup.display()
-    },
-    onClearSelected() {
-      this.selectedRowKeys = [];
-      this.selectionRows = [];
-      this.resetOrderData()
-    },
-    resetOrderData() {
-      this.orderData = {
-        skuNumber: 0,
-        totalQuantity: 0,
-        estimatedTotalPrice: 0,
-        reducedAmount: 0
-      }
+    callback(key) {
+      console.log(key);
     }
   }
 }
 </script>
-<style lang="less" scoped>
-@import '~@assets/less/common.less';
-.bottomButtons {
-  margin-left: 88%;
-}
-</style>
