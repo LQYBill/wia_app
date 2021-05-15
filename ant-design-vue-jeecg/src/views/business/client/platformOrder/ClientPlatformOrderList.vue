4cee5a1c63e57b84989e35131aed788263de7899
Qiuyi LI <qiuyi.li@gmail.com>
2021-05-15 12:25:34 +0200

    Feature : display order quantities on client platform order page

--- ant-design-vue-jeecg/src/views/business/client/platformOrder/ClientPlatformOrderList.vue
+++ ant-design-vue-jeecg/src/views/business/client/platformOrder/ClientPlatformOrderList.vue
@@ -2,19 +2,19 @@
   <a-tabs default-active-key="1" @change="callback">
     <a-tab-pane key="1">
       <span slot="tab">
-        Pending orders <a-badge :count="109" :number-style="{ backgroundColor: '#fa541c' }" :overflow-count="999" style='margin-bottom: 5px'/>
+        Pending orders <a-badge show-zero :count="orderQuantity.pending" :number-style="{ backgroundColor: '#fa541c' }" :overflow-count="999" style='margin-bottom: 5px'/>
       </span>
       <pending-order-list/>
     </a-tab-pane>
     <a-tab-pane key="2" force-render>
       <span slot="tab">
-        Purchasing orders <a-badge count="109" :number-style="{ backgroundColor: '#faad14' }" :overflow-count="999" style='margin-bottom: 5px'/>
+        Purchasing orders <a-badge show-zero :count="orderQuantity.purchasing" :number-style="{ backgroundColor: '#faad14' }" :overflow-count="999" style='margin-bottom: 5px'/>
       </span>
       Content of Tab Pane 2
     </a-tab-pane>
     <a-tab-pane key="3">
       <span slot="tab">
-        Processed orders <a-badge count="109" :number-style="{ backgroundColor: '#a0d911' }" :overflow-count="999" style='margin-bottom: 5px'/>
+        Processed orders <a-badge show-zero :count="orderQuantity.processed" :number-style="{ backgroundColor: '#a0d911' }" :overflow-count="999" style='margin-bottom: 5px'/>
       </span>
       Content of Tab Pane 3
     </a-tab-pane>
@@ -24,7 +24,7 @@
 <script>
 
 import PendingOrderList from './PendingOrderList'
-
+import { getAction } from '@api/manage'
 import '@assets/less/TableExpand.less'
 
 export default {
@@ -35,13 +35,33 @@ export default {
   data() {
     return {
       description: 'Client platform order page',
+      url: {
+        getData: '/business/clientPlatformOrder/queryQuantities'
+      },
+      orderQuantity: {
+        processed: 0,
+        pending: 0,
+        purchasing: 0
+      }
     }
   },
   created() {
-
+    this.loadData()
   },
   computed: {},
   methods: {
+    loadData() {
+      this.loading = true
+      getAction(this.url.getData).then(res => {
+        if (res.success) {
+          this.orderQuantity = res.result
+        } else {
+          this.$error({title: 'Request failed', content: res.message})
+        }
+      }).finally(() => {
+        this.loading = false
+      })
+    },
     callback(key) {
       console.log(key);
     }
