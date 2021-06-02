14dd4d3925a3f7130d108f640af264ae74ba2fe6
Wenke-D <matthieu.du@outlook.com>
2021-06-02 17:45:30 +0200

    add a test for sending request to order list

--- jeecg-boot/jeecg-boot-module-system/src/test/java/org/jeecg/modules/business/org/jeecg/modules/business/domain/mabangapi/OrderListRequestTest.java
+++ jeecg-boot/jeecg-boot-module-system/src/test/java/org/jeecg/modules/business/org/jeecg/modules/business/domain/mabangapi/OrderListRequestTest.java
@@ -1,8 +1,16 @@
 package org.jeecg.modules.business.org.jeecg.modules.business.domain.mabangapi;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.jeecg.modules.business.domain.mabangapi.DateType;
 import org.jeecg.modules.business.domain.mabangapi.OrderListRequest;
+import org.jeecg.modules.business.domain.mabangapi.OrderListRequestBody;
+import org.jeecg.modules.business.domain.mabangapi.OrderStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,8 +19,14 @@ public class OrderListRequestTest {
 
     @Test
     void testToReq(){
-        ResponseEntity<String> res = OrderListRequest.toReq();
-        String expect = "ae6dbd7743e05cafb8b3b17b7f11d4379a2392a8da119acf66082a3262651f96";
-        System.out.println(res);
+        OrderListRequestBody body = new OrderListRequestBody();
+        body.setStatus(OrderStatus.Purchasing)
+                .setStartDateType(DateType.UPDATE)
+                .setStartDate(new Date(2020, Calendar.JULY, 1))
+                .setEndDateType(DateType.UPDATE)
+                .setEndDate(new Date(2020, Calendar.JULY, 2));
+        ResponseEntity<String> res = OrderListRequest.sentRequest(body);
+        JSONObject resBody = JSON.parseObject(res.getBody());
+        assertEquals("000",resBody.get("code"));
     }
 }
