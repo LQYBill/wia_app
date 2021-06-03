9e0713279e0c75b5be54e72230cc565b91d76436
Wenke-D <matthieu.du@outlook.com>
2021-06-03 18:09:16 +0200

    a little bit of schedule task

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/business/domain/mabangapi/getorderlist/RetrieveOrderList.java
@@ -0,0 +1,16 @@
+package org.jeecg.modules.business.domain.mabangapi.getorderlist;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class RetrieveOrderList implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+    }
+
+    private void updateDateFromMabang(){
+
+    }
+}
