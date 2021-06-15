5f483b95e0a5b8ce48f7286b3dde52a7ad535424
Wenke-D <matthieu.du@outlook.com>
2021-06-15 17:58:04 +0800

    write test

--- jeecg-boot/jeecg-boot-module-system/src/test/java/org/jeecg/modules/business/org/jeecg/modules/business/domain/mabangapi/getorderlist/RetrieveOrderListJobTest.java
+++ jeecg-boot/jeecg-boot-module-system/src/test/java/org/jeecg/modules/business/org/jeecg/modules/business/domain/mabangapi/getorderlist/RetrieveOrderListJobTest.java
@@ -1,27 +1,30 @@
 package org.jeecg.modules.business.org.jeecg.modules.business.domain.mabangapi.getorderlist;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import org.jeecg.modules.business.domain.mabangapi.getorderlist.Order;
-import org.jeecg.modules.business.domain.mabangapi.getorderlist.OrderItem;
+import org.jeecg.modules.business.domain.mabangapi.getorderlist.OrderListRequestErrorException;
 import org.jeecg.modules.business.domain.mabangapi.getorderlist.RetrieveOrderListJob;
+import org.jeecg.modules.business.service.impl.PlatformOrderServiceImpl;
+import org.jeecg.modules.business.service.impl.purchase.PlatformOrderContentServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.List;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.notNull;
+import static org.mockito.Mockito.mock;
 
 
 public class RetrieveOrderListJobTest {
 
 
     @Test
-    void updateDateFromMabangTest() {
+    void updateDateFromMabangTest() throws OrderListRequestErrorException {
+        PlatformOrderContentServiceImpl fakeContentService = mock(PlatformOrderContentServiceImpl.class);
+        PlatformOrderServiceImpl fakeService = mock(PlatformOrderServiceImpl.class);
 
+        RetrieveOrderListJob job = new RetrieveOrderListJob();
+        job.setPlatformOrderContentService(fakeContentService);
+        job.setPlatformOrderService(fakeService);
+
+        job.updateMergedOrder();
     }
 
 }
