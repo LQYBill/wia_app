94c566765b1fbc0040c1f58865287063ec944fc1
Qiuyi LI <qiuyi.li@gmail.com>
2021-07-05 14:27:32 +0200

    Feature : add script for view detail_de_facture

---
+++ jeecg-boot/db/views/detail_de_facture.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE VIEW detail_de_facture AS
+SELECT s.name                      AS 'Boutique',
+       po.platform_order_id        AS 'N° de Mabang',
+       po.platform_order_number    AS 'N° de commande',
+       po.tracking_number          AS 'N° de suivi',
+       po.order_time               AS 'Date de commande',
+       po.shipping_time            AS 'Date d\'expédition',
+       po.recipient                AS 'Nom de client',
+       po.country                  AS 'Pays',
+       po.postcode                 AS 'Code postal',
+       JSON_ARRAYAGG(sku.erp_code) AS 'SKU',
+       JSON_ARRAYAGG(poc.quantity) AS 'Quantité',
+       po.fret_fee                 AS 'Frais de FRET',
+       SUM(poc.shipping_fee)       AS 'Frais de livraison',
+       SUM(poc.vat)                AS 'TVA',
+       po.shipping_invoice_number  AS 'N° de facture'
+FROM platform_order po
+         JOIN shop s ON po.shop_id = s.id
+         RIGHT JOIN platform_order_content poc ON po.id = poc.platform_order_id
+         JOIN sku ON poc.sku_id = sku.id
+WHERE shipping_invoice_number IS NOT NULL
+GROUP BY po.id, s.name, po.order_time
+ORDER BY s.name, po.order_time;
