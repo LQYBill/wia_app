508e0d86787489923868121cfa8620ec12c3f242
Wenke-D <matthieu.du@outlook.com>
2021-07-14 13:52:14 +0800

    new view

--- jeecg-boot/jeecg-boot-module-system/src/main/resources/sql/views.sql
+++ jeecg-boot/jeecg-boot-module-system/src/main/resources/sql/views.sql
@@ -4,4 +4,14 @@ SELECT country, c.name_zh, c.code, count(*) AS 'quantity'
 FROM platform_order po
          JOIN country c on po.country = c.name_en
 GROUP BY country
-ORDER BY count(*) DESC
+ORDER BY count(*) DESC;
+
+# user_sku
+CREATE VIEW user_sku AS
+SELECT uc.user_id, spn.id AS 'sku_id', spn.erp_code, zh_name, en_name
+FROM client c
+         JOIN user_client uc ON c.id = uc.client_id
+         JOIN client_sku cs ON c.id = cs.client_id
+         JOIN sku s ON cs.sku_id = s.id
+         JOIN sku_product_name spn ON s.id = spn.id
+ORDER BY uc.user_id
