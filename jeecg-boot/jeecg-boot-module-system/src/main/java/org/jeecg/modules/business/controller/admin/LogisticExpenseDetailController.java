b2186874064073538f4c94a4c72f9d9e4e6c465a
Qiuyi LI <qiuyi.li@gmail.com>
2021-08-16 16:06:15 +0200

    Minor fix

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/business/controller/admin/LogisticExpenseDetailController.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/business/controller/admin/LogisticExpenseDetailController.java
@@ -286,7 +286,6 @@ public class LogisticExpenseDetailController extends JeecgController<LogisticExp
                 detail.setShippingFee(new BigDecimal(exceptionWrapper("运费", lineData)));
                 detail.setDiscount(new BigDecimal(exceptionWrapper("运费优惠金额", lineData)));
                 detail.setFuelSurcharge(new BigDecimal(exceptionWrapper("燃油附加费", lineData)));
-                detail.setAdditionalFee(new BigDecimal(exceptionWrapper("速递运费类", lineData)));
                 detail.setSecondDeliveryFee(new BigDecimal(exceptionWrapper("重派费", lineData)));
                 detail.setRegistrationFee(new BigDecimal(exceptionWrapper("挂号费", lineData)));
                 detail.setVat(new BigDecimal(exceptionWrapper("代收VAT税", lineData)));
